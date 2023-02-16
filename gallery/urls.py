7d224fa81bea516ff0ade84003080d3f1aed5934
Lucas Caldas <lucasmedeiroscaldas@yahoo.com.br>
2023-02-16 20:14:33 -0300

    add search bar functional

--- gallery/urls.py
+++ gallery/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from gallery.views import index, imagem
+from gallery.views import index, imagem, buscar
 
 
 urlpatterns = [
     path('', index, name = 'index'),
     path('imagem/<int:foto_id>', imagem, name='imagem'),
+    path("buscar", buscar, name="buscar"),
 ]
